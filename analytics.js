a5ac24ffe82d676fbfcc795250feaa785b8984d2
tit4naies-prog <tit4naies@gmail.COM>
2026-07-10 21:25:52 +0200

    auto-deploy

--- analytics.js
+++ analytics.js
@@ -1,16 +1,17 @@
 (function () {
+  var ns = "shadow4stories-arch.autonich";
+  var page = location.pathname;
+  var img = new Image();
+  img.src = "https://api.countapi.xyz/hit/" + ns + "/pv";
+  var img2 = new Image();
+  img2.src = "https://api.countapi.xyz/hit/" + ns + "/page" + btoa(page).replace(/=/g, "");
   var uuid = localStorage.getItem("_auid");
   if (!uuid) {
     uuid = crypto.randomUUID ? crypto.randomUUID() : Math.random().toString(36).slice(2) + Date.now().toString(36);
     localStorage.setItem("_auid", uuid);
   }
-  var page = location.pathname;
-  var ref = document.referrer || "";
-  var api = "http://127.0.0.1:9090";
-  var img = new Image();
-  img.src = api + "/count?p=" + encodeURIComponent(page) + "&v=" + encodeURIComponent(uuid) + "&r=" + encodeURIComponent(ref);
   var stored = JSON.parse(localStorage.getItem("_avisits") || "[]");
-  stored.push({ p: page, t: Date.now(), r: ref });
+  stored.push({ p: page, t: Date.now(), r: document.referrer || "" });
   if (stored.length > 200) stored = stored.slice(-200);
   localStorage.setItem("_avisits", JSON.stringify(stored));
 })();
